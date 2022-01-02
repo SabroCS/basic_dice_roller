40989eb5b8ade09054b71e008b588e2d9a3a8269
Sabrina <scamara@digitalresponse.es>
2022-01-02 15:43:30 +0100

    bugfixes

--- scripts.js
+++ scripts.js
@@ -2,20 +2,9 @@ function dice4() {
     var dice4 = "4 dice";
     var dice4Value;
 
-    dice4Value = (Math.ceil(Math.random() * 4));
+    dice4Value = (Math.floor(Math.random() * 4));
 
     console.log (("Your roll of ")+dice4+(": ")+dice4Value);
     
     document.getElementById("Dice4").textContent=dice4Value;
   }  
-
-function dice8() {
-    var dice8 = "8 dice";
-    var dice8Value;
-
-    dice8Value = (Math.ceil(Math.random() * 8));
-
-    console.log (("Your roll of ")+dice8+(": ")+dice8Value);
-    
-    document.getElementById("Dice8").textContent=dice8Value;
-  } 
